22414d703ad9ba1afd824f1f691f1ad4515d9489
Jeremy Raines <jraines@gmail.com>
2013-01-15 21:25:58 -0800

    update API endpoint to prod site

--- lib/omniauth/strategies/stylitics.rb
+++ lib/omniauth/strategies/stylitics.rb
@@ -9,7 +9,7 @@ module OmniAuth
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
-      option :client_options, {:site => "http://stylitics-qa.herokuapp.com",
+      option :client_options, {:site => "https://www.stylitics.com",
                                :authorize_url => '/oauth/authorize',
                                :token_url => '/oauth/token'}
 
